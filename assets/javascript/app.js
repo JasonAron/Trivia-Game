816dd8756bc8e66629b919539b27ad4dee4a5657
Jason M. Aron <jasonaronart@gmail.com>
2018-02-16 17:43:44 -0500

    Trivia-Game-2.0

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -54,8 +54,6 @@
 //===============================
 
 
-var startScreen
-var counter = 25;
 var questionArray = [""]
 var answerArray = [[""]]
 var correctAnswers = [""]
@@ -63,30 +61,82 @@ var correctTotal = 0;
 var incorrectTotal = 0;
 var unansweredTotal = 0;
 
+//====================
+// ---- Questions ----
+//====================
 let state = {
 	myQuestions: [
 		{
 			question: "Which of the following groups is disappearing the fastest?",
 			answers:[
-				 "Land species",
-				 "Sea species",
-				 "Fresh water",
+				 "Land Species",
+				 "Sea Species",
+				 "Fresh Water Species",
 			],
-			correctAnswer: "c"
+			correctAnswer: [2]
 		},
 
 		{
-			question: "How many people could we feed with the grain we use for the cattle industry?",
+			question: "How many people could we feed with the grain we use for the Animal Agriculture Industry?",
 			answers: [
-				 "Land species",
-				 "Sea species",
-				 "Fresh water",
+				 "1 Million",
+				 "800 Million",
+				 "2 Billion",
+				 "9 Billion"
 			],
-			correctAnswer: "c"
+			correctAnswer: [3]
+		},
+		{
+			question: "What is the number one driving force behind deforestation?",
+			answers: [
+				 "Timber and paper",
+				 "Palm Oil",
+				 "Cattle and pasture",
+			],
+			correctAnswer: [2]
+		},
+		{
+			question: "Which of these industry giants uses more water?",
+			answers: [
+				 "Crop",
+				 "Animal Agriculture",
+				 "Fracking",
+			],
+			correctAnswer: [1]
+		},
+		{
+			question: "What mass extinction did our planet just enter?",
+			answers: [
+				 "3rd",
+				 "4th",
+				 "5th",
+				 "6th"
+			],
+			correctAnswer: [3]
+		},	
+		{
+			question: "How many toxins are regularly found in your drinking water?",
+			answers: [
+				 "4",
+				 "6",
+				 "8",
+				 "12"
+			],
+			correctAnswer: [3]
+		},
+		{
+			question: "How much of the world's wealth do the riches 1% own?",
+			answers: [
+				 "20%",
+				 "30%",
+				 "40%",
+				 "50%"
+			],
+			correctAnswer: [3]
 		},
 	],
 	questionCounter: 0
-} 
+}; 
 
 
 
@@ -97,12 +147,23 @@ function startGame() {
 
 };
 
+function startTimer() {
+	var timeleft = 25;
+	var createTimer = setInterval(function(){
+	timeleft--;
+	document.getElementById("countdowntimer").textContent = timeleft;
+	if(timeleft <= 0)
+    	clearInterval(createTimer);
+	},1000);
+};
+
 function generateQuestions() {
 	var currentQuestion = state.myQuestions[state.questionCounter]
 	$(".question").html(currentQuestion.question)
 	for (let i = 0; i < currentQuestion.answers.length; i++) {
-		$(".answers").append("<button class='answer' >" + currentQuestion.answers[i] + "</button>"); 		
+		$(".answers").append("<button class='answer'>" + currentQuestion.answers[i] + "</button>" + "<br>");		
 	}
+	startTimer()
 };
 
 $(".startBtn").on("click", function() {
